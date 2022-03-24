94b9388e67faeaa5970e51fe62a83406ed4b5b7e
Victor-Aremu <victoraremu123@gmail.com>
2022-03-24 20:24:52 +0000

    responsive

--- src/views/DetailsView.vue
+++ src/views/DetailsView.vue
@@ -21,10 +21,10 @@ export default {
     class="md:h-screen overflow-hidden relative flex items-center justify-between max-w-full px-8 md:px-16 font-[Poppins] border-b z-20 bg-[#f0f5f5]"
   >
     <div
-      class="relative w-full md:w-[62%] h-full md:h-[700px] border-4 md:border-r-[#54c278] mx-auto py-12 px-4 shadow-lg rounded-lg bg-[#64371f]"
+      class="relative w-full md:w-[62%] h-full lg:h-[auto] 4xl:h-[700px] border-4 md:border-r-[#54c278] mx-auto py-12 px-4 shadow-lg rounded-lg bg-[#64371f]"
     >
       <div
-        class="md:absolute relative w-52 md:mt-0 mx-auto md:w-96 md:h-full object-fill md:-top-10 md:-left-32"
+        class="md:absolute relative w-52 md:mt-0 mx-auto lg:w-80 4xl:w-96 md:h-full object-fill md:-top-10 md:-left-32"
       >
         <img
           class="rounded-lg shadow-lg"
@@ -32,7 +32,7 @@ export default {
           alt="Profile"
         />
       </div>
-      <div class="md:ml-72 ml-2 relative mt-4 md:mt-0">
+      <div class="4xl:ml-72 md:ml-48 ml-2 relative mt-4 md:mt-0">
         <div
           class="md:absolute md:-left-32 top-0 md:text-8xl text-2xl font-[Satisfy] text-white flex items-center"
         >
@@ -56,7 +56,7 @@ export default {
           <div>{{ student.special }} Developer</div>
         </div>
         <!-- Skills -->
-        <div class="text-white md:mt-8 mt-2 md:text-2xl text-lg">
+        <div class="text-white 4xl:mt-8 mt-2 md:text-2xl text-lg">
           <div class="text-[#c8c8c1] tracking-wide font-semibold">Skills</div>
           <div class="flex gap-5 text-sm">
             <ul v-for="skill in student.skills" :key="skill">
@@ -69,9 +69,9 @@ export default {
           </div>
         </div>
         <!-- About -->
-        <div class="md:pt-14 pt-4 text-white">
+        <div class="4xl:pt-14 pt-4 text-white">
           <div
-            class="md:text-2xl text-lg font-semibold tracking-wide text-[#c8c8c1]"
+            class="4xl:text-2xl md:text-xl text-lg font-semibold tracking-wide text-[#c8c8c1]"
           >
             About
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "4xl": "1920px",
+      },
+    },
   },
   plugins: [],
 };
